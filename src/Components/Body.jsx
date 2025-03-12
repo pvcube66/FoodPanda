209f45363e03085e91eeb67d4db5e5d55dfbea67
Vishnu Pavali <yoursvishnation@gmail.com>
2025-03-12 21:20:14 +0530

    hello trying to fix cors

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,70 +1,44 @@
-import { useState,useEffect } from 'react';
-import { restuarantList } from './config';
-import RestaurantCard from './RestaurantCard';
-import Shimmer from './Shimmer';
-import { Link } from 'react-router-dom';
-// import { getFilteredData } from './HeaderComponent';
-
-import { withOpenLabel } from './RestaurantCard';
-
-export function filterData(searchText, restaurants) {
-  return restaurants.filter((restaurant) =>
-    restaurant.info.name.toLowerCase().includes(searchText.toLowerCase())
-  );
-}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import RestaurantCard, { withOpenLabel } from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  // const [searchText, setSearchText] = useState("");
   const [restaurants, setRestaurants] = useState([]);
+  const RestaurantCardOpened = withOpenLabel(RestaurantCard);
 
-  const RestaurantCardOpened=withOpenLabel(RestaurantCard);
+  useEffect(() => {
+    getRestaurants();
+  }, []);
 
-  useEffect(()=>{
-    getRestaurants()
-  },)
- 
- 
-
-  //if restaurants is empty => shimmer ui
-  // if restaurant has data => actual data ui
-  
   async function getRestaurants() {
-    const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.9324914&lng=83.42679729999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-    // console.log(response)
-    const newData = await response.json();
-    // console.log(newData);
-    const restaurantsData = newData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    const restaurantsData2=newData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    if (restaurantsData) {
-      // const limitedRestaurants = restaurantsData.slice(0, 8);
-      setRestaurants(restaurantsData);
-      // console.log(restaurantsData)
-    }
-    else if(restaurantsData2){
-      setRestaurants(restaurantsData2);
-      // console.log(restaurantsData2)
+    try {
+      const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.9324914&lng=83.42679729999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      if (!response.ok) throw new Error("Failed to fetch data");
 
-    } 
-    else {
-      console.error("Failed to fetch restaurants data");
+      const newData = await response.json();
+      const restaurantsData = newData?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      const restaurantsData2 = newData?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+      setRestaurants(restaurantsData || restaurantsData2 || []);
+    } catch (error) {
+      console.error("Fetch error:", error);
     }
   }
-  return (restaurants.length===0)?<Shimmer/> : (
-    <>
-   
 
-      <div className='body'>
-        {restaurants.map((restaurant) => (
-        <Link to={"restaurant/" + restaurant.info.id}  key={restaurant.info.id}> 
-      {
-          (restaurant?.info?.isOpen==true)?<RestaurantCardOpened props={restaurant}/>: <RestaurantCard props={restaurant}  /> 
-      }
-        
-          </Link>
-        ))}
-      </div> 
-    </>
+  return restaurants.length === 0 ? <Shimmer /> : (
+    <div className="body">
+      {restaurants.map((restaurant) => (
+        <Link to={"restaurant/" + restaurant.info.id} key={restaurant.info.id}>
+          {restaurant?.info?.isOpen ? (
+            <RestaurantCardOpened props={restaurant} />
+          ) : (
+            <RestaurantCard props={restaurant} />
+          )}
+        </Link>
+      ))}
+    </div>
   );
-}
+};
 
 export default Body;
